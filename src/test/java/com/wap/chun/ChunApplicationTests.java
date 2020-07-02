4ef52a1622ceb46df9537f1a6427505fc6ff54f2
ParkChunSo <cnsth123@gmail.com>
2020-07-02 22:55:35 +0900

    applicatioin init

---
+++ src/test/java/com/wap/chun/ChunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wap.chun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c776df0fe578d89d06b680b985a856a195a67637
ParkChunSo <cnsth123@gmail.com>
2020-07-03 16:59:34 +0900

    add jpa update domain

--- src/main/java/com/wap/chun/domain/entitys/Board.java
+++ src/main/java/com/wap/chun/domain/entitys/Board.java
@@ -1,11 +1,33 @@
 package com.wap.chun.domain.entitys;
 
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity(name = "board_tbl")
+@Getter
 public class Board {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "club_id")
     private Club club;
+    @ManyToOne
+    @JoinColumn(name = "writer_id")
     private Member writer;
+    @CreationTimestamp
     private LocalDateTime createDate;
     private String message;
+
+    public Board(Club club, Member writer, String message) {
+        this.club = club;
+        this.writer = writer;
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- src/main/java/com/wap/chun/domain/entitys/Post.java
+++ src/main/java/com/wap/chun/domain/entitys/Post.java
@@ -1,17 +1,54 @@
 package com.wap.chun.domain.entitys;
 
+import com.sun.istack.NotNull;
 import com.wap.chun.domain.enums.PostCategory;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity(name = "post_tbl")
+@Getter
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Enumerated(value = EnumType.STRING)
     private PostCategory category;
     private String location;
-    private Club clubId;
+    @ManyToOne
+    @JoinColumn(name = "club_id")
+    private Club club;
+    @ManyToOne
+    @JoinColumn(name = "writer_id")
     private Member writer;
+    @CreationTimestamp
     private LocalDateTime createDate;
+    @NotNull
     private LocalDateTime startDate;
+    @NotNull
     private LocalDateTime endDate;
     private String message;
+
+    public Post(PostCategory category, Club club, Member writer, LocalDateTime startDate, LocalDateTime endDate) {
+        this.category = category;
+        this.club = club;
+        this.writer = writer;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public void setCategory(PostCategory category) {
+        this.category = category;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }

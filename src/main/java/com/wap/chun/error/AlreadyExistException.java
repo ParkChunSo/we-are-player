5f81dca6b0f66c356a2eed653bb098521064834f
DESKTOP-2389N04\chun <cnsth123@gmail.com>
2020-07-07 14:26:39 +0900

    update exception class

---
+++ src/main/java/com/wap/chun/error/AlreadyExistException.java
@@ -0,0 +1,4 @@
+package com.wap.chun.error;
+
+public class AlreadyExistException extends RuntimeException{
+}
---
+++ src/main/java/com/wap/chun/error/ClubAlreadyExistException.java
@@ -0,0 +1,4 @@
+package com.wap.chun.error;
+
+public class ClubAlreadyExistException extends AlreadyExistException {
+}
